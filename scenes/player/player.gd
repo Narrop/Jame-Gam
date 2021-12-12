a7fe5ef7615767cc4f419830d46753c2c579f5cf
Narrop <nb30@hotmail.fr>
2021-12-12 09:15:58 +0100

    player
    Ajout du changement de sens sur le joueur plus changement d'animation

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,16 +8,25 @@ var velocity = Vector2.ZERO
 
 func get_input():
 	velocity = Vector2.ZERO
+	var zero = Vector2.ZERO
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed('ui_left'):
 		velocity.x -= 1
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed('ui_down'):
 		velocity.y += 1
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
-	# Make sure diagonal movement isn't faster
-
+	
+	# change l'animation en fonction du déplacement du joueur
+	if velocity == zero:
+		$AnimatedSprite.animation = "standing"
+	else:
+		$AnimatedSprite.animation = "running"
+	
+	# on normalisent les mouvement afin qu'il se fassent tous à la meme vitesse
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
