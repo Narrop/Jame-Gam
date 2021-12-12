26a2bdf7c7d0f9606afa37540c9e4e00fe4a917a
Narrop <nb30@hotmail.fr>
2021-12-12 11:12:21 +0100

    ûsh

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,13 +5,15 @@ extends KinematicBody2D
 #variable globale du script
 var speed = 200 
 var velocity = Vector2.ZERO
+var on_object = null
+var got_key = false
+
 
 func get_input():
 	
 	velocity = Vector2.ZERO
-	var zero = Vector2.ZERO
 	
-	
+	#
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
 		$AnimatedSprite.flip_h = false
@@ -23,15 +25,32 @@ func get_input():
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
 	
-	# change l'animation en fonction du déplacement du joueur
+	# on normalisent les mouvement afin qu'il se fassent tous à la meme vitesse
+	velocity = velocity.normalized() * speed
+	
+	if Input.is_action_pressed("ui_select") and on_object != null:
+		if (on_object.is_in_group("KEY")):
+			got_key = true
+			get_tree().call_group("KEY", "queue_free")
+
+func _on_detector_area_entered(area):
+	on_object = area
+
+
+func _on_detector_area_exited(area):
+	on_object = null
+
+func _physics_process(delta):
+	
+	var zero = Vector2.ZERO
+	
+	get_input()
+	
+		# change l'animation en fonction du déplacement du joueur
 	if velocity == zero:
 		$AnimatedSprite.animation = "standing"
 	else:
 		$AnimatedSprite.animation = "running"
 	
-	# on normalisent les mouvement afin qu'il se fassent tous à la meme vitesse
-	velocity = velocity.normalized() * speed
-
-func _physics_process(delta):
-	get_input()
+	#fonction qui fait bouger un kinematic body
 	velocity = move_and_slide(velocity)
