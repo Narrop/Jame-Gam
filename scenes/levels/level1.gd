39278d8417a767f25acc682a38071b7dedaaf11e
Narrop <nb30@hotmail.fr>
2021-12-12 17:30:22 +0100

    marie push

--- scenes/levels/level1.gd
+++ scenes/levels/level1.gd
@@ -9,6 +9,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$player/Text.visible = true
+	$music_menu_tuto.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
