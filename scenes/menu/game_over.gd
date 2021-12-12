3f25e607c9a82249e387ac5d612fba5f18c78aa1
Narrop <nb30@hotmail.fr>
2021-12-12 19:25:35 +0100

    réparation game over

--- scenes/menu/game_over.gd
+++ scenes/menu/game_over.gd
@@ -10,10 +10,6 @@ func _ready():
 	$Label.set("font_color",Color(0.447,0.839,0.807))
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Timer_timeout():
 	$Label.set("custom_colors/font_color",Color(0.854,0.305,0.219))
