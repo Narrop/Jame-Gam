93e8c54ad818fc5e1254e06e9d0c97072fda6e7d
MarieDNF <marie.dintrono@gmail.com>
2021-12-12 19:13:38 +0100

    mouahahaha

--- scenes/menu/game_over.gd
+++ scenes/menu/game_over.gd
@@ -2,15 +2,13 @@ extends Control
 
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.start()
-	#$Label.font_color = Color(1,1,1,1)
 	$Label.set("font_color",Color(0.447,0.839,0.807))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -19,8 +17,8 @@ func _ready():
 
 func _on_Timer_timeout():
 	$Label.set("custom_colors/font_color",Color(0.854,0.305,0.219))
-	#$Label.set("custom_colors/font_color", Color(1,0,0))
-	#	$Label.font_color = Color(218,78,56,207)
+	$mouahaha.play()
+
 
 
 func _on_echap_pressed():
